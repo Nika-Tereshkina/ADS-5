0868c390b53b60de0541f29ab6ba17975956d06c
Nika-Tereshkina <159996107+Nika-Tereshkina@users.noreply.github.com>
2024-04-04 19:44:05 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,53 +3,53 @@
 #include <map>
 #include "tstack.h"
 int GetPriority(char c) {
-    switch (c) {
-        case '(':
-            return 0;
-        case ')':
-            return 1;
-        case '+':
-        case '-':
-            return 2;
-        case '*':
-        case '/':
-            return 3;
-        default:
-            return -1;
-   }
+switch (c) {
+    case '(':
+        return 0;
+    case ')':
+        return 1;
+    case '+':
+    case '-':
+        return 2;
+    case '*':
+    case '/':
+        return 3;
+    default:
+        return -1;
+    }
 }
 std::string infx2pstfx(std::string inf) {
-    std::string postfix, temp;
-    TStack<char, 100> operators;
-    for (auto& ch : inf) {
+std::string postfix, temp;
+TStack<char, 100> operators;
+for (auto& ch : inf) {
     int priority = GetPriority(ch);
     if (priority == -1) {
-            postfix += ch;
-            postfix += ' ';
+        postfix += ch;
+        postfix += ' ';
+    } else {
+        char top = operators.get();
+        if (priority == 0 || GetPriority(top) < priority || operators.isEmpty()) {
+            operators.push(ch);
         } else {
-            char top = operators.get();
-            if (priority == 0 || GetPriority(top) < priority || operators.isEmpty()) {
-                operators.push(ch);
-            } else {
-                if (ch == ')') {
-                    while (GetPriority(top) >= priority) {
-                        postfix += top;
-                        postfix += ' ';
-                        operators.pop();
-                        top = operators.get();
-                    }
+            if (ch == ')') {
+                while (GetPriority(top) >= priority) {
+                    postfix += top;
+                    postfix += ' ';
                     operators.pop();
+                    top = operators.get();
+                }
+                operators.pop();
                 } else {
                     while (GetPriority(top) >= priority) {
                         postfix += top;
                         postfix += ' ';
                         operators.pop();
                         top = operators.get();
-                    }
-                    operators.push(ch);
+                     }
+                     operators.push(ch);
                 }
             }
-        }
+       }
     }
     while (!operators.isEmpty()) {
         postfix += operators.get();
@@ -58,42 +58,42 @@ std::string infx2pstfx(std::string inf) {
     }
     return postfix;
 }
-int calculate(const int& op1, const int& op2, const int& oper) {
-    switch (oper) {
-        case '+':
-            return op1 + op2;
-        case '-':
-            return op1 - op2;
-        case '/':
-            return op1 / op2;
-        case '*':
-            return op1 * op2;
-        default:
-            return 0;
+int calculate(const int& op1, const int& op2, const char& oper) {
+switch (oper) {
+    case '+':
+        return op1 + op2;
+    case '-':
+        return op1 - op2;
+    case '/':
+        return op1 / op2;
+    case '*':
+        return op1 * op2;
+    default:
+        return 0;
     }
 }
 int eval(std::string pref) {
-    TStack<int, 100> operands;
-    std::string temp = "";
-    for (int i = 0; i < pref.size(); i++) {
-        char ch = pref[i];
-        if (GetPriority(ch) == -1) {
-            if (pref[i] == ' ') {
-                continue;
-            } else if (isdigit(pref[i+1])) {
-                temp += pref[i];
-                continue;
-            } else {
-                temp += pref[i];
-                operands.push(atoi(temp.c_str()));
-                temp = "";
-            }
+TStack<int, 100> operands;
+std::string temp = "";
+for (int i = 0; i < pref.size(); i++) {
+    char ch = pref[i];
+    if (GetPriority(ch) == -1) {
+        if (pref[i] == ' ') {
+            continue;
+        } else if (std::isdigit(pref[i+1])) {
+            temp += pref[i];
+            continue;
         } else {
-            int op2 = operands.get();
-            operands.pop();
-            int op1 = operands.get();
-            operands.pop();
-            operands.push(calculate(op1, op2, ch));
+            temp += pref[i];
+            operands.push(atoi(temp.c_str()));
+            temp = "";
+        }
+    } else {
+        int op2 = operands.get();
+        operands.pop();
+        int op1 = operands.get();
+        operands.pop();
+        operands.push(calculate(op1, op2, ch));
         }
     }
     return operands.get();
